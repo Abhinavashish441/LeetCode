62297e76d2496955825dc36e4fbb1f3d2168079d
Abhinav Ashish <94304669+Abhinavashish441@users.noreply.github.com>
2022-01-30 12:03:52 +0530

    Added solution - LeetHub

---
+++ Painting the Fence - GFG/painting-the-fence.java
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+ // } Driver Code Ends
+//User function Template for Java
+
+
+class Solution
+{
+    long countWays(int n,int k)
+    {
+        if(n==1) return (long)k;
+        long m = 1000000007;
+        long same = (k*1)%m;
+        long diff = (k*(k-1))%m;
+        long total = (same + diff)%m;
+        for(int i=3;i<=n;i++){
+            same = (diff*1)%m;
+            diff = (total*(k-1))%m;
+            total = (same + diff)%m;
+        }
+        return (total)%m;
+    }
+}
+
+
+
+
+// { Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main (String[] args) throws IOException{
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while(testcases-- > 0){
+            //int n=Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] a2 = line.trim().split("\\s+");
+            int n =Integer.parseInt(a2[0]);
+            int k =Integer.parseInt(a2[1]);
+
+            Solution ob=new Solution();
+            //ArrayList<Long> ans=ob.smallestDifferenceTriplet(a,b,c,n);
+            long ans=ob.countWays(n,k);
+            System.out.println(ans);
+        }
+    }
+}
+
+
+  // } Driver Code Ends
