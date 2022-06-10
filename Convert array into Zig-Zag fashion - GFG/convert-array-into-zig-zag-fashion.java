c26a2b94539d3ff7a3c5ab79110f8293e8db5d09
Abhinav Ashish <94304669+Abhinavashish441@users.noreply.github.com>
2022-06-10 17:21:57 +0530

    Added solution - LeetHub

---
+++ Convert array into Zig-Zag fashion - GFG/convert-array-into-zig-zag-fashion.java
@@ -0,0 +1,57 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().zigZag(arr, n);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    void zigZag(int arr[], int n) {
+        // code here
+        for(int i =0;i<n-1;i++){
+            if(i%2 != 0){
+                if(arr[i]<arr[i+1]){
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                }
+            }
+            else{
+                if(arr[i]>arr[i+1]){
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                }
+            }
+        }
+    }
+
+}
