1efaadb04e2ced9b8b8f724a7b1d86c6d68ec01c
Abhinav Ashish <94304669+Abhinavashish441@users.noreply.github.com>
2022-06-11 16:30:20 +0530

    Added solution - LeetHub

---
+++ Red OR Green - GFG/red-or-green.java
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+// initial template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Position this line where user code will be pasted.
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            String S = read.readLine();
+            Solution ob = new Solution();
+            System.out.println(ob.RedOrGreen(N, S));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function template for Java
+
+class Solution {
+    static int RedOrGreen(int N, String S) {
+        //code here
+        int r=0;
+        int g=0;
+        for(int i=0;i<N;i++){
+            if(S.charAt(i)=='R'){
+                r++;
+            }
+            else if(S.charAt(i)=='G'){
+                g++;
+            }
+        }
+        return Math.min(r,g);
+    }
+}
