080693f80520c3c07e514835a388bd32cd6722be
Abhinav Ashish <94304669+Abhinavashish441@users.noreply.github.com>
2022-06-12 14:56:54 +0530

    Added solution - LeetHub

---
+++ Intersection of two arrays - GFG/intersection-of-two-arrays.java
@@ -0,0 +1,74 @@
+// { Driver Code Starts
+// Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+    public static void main(String[] args) {
+
+        // Taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Taking count of total testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n, m;
+
+            // taking count of elements in array a
+            n = sc.nextInt();
+
+            // taking count of elements in array b
+            m = sc.nextInt();
+
+            // Creating 2 arrays of n and m
+            int a[] = new int[n];
+            int b[] = new int[m];
+
+            // inserting elements to array a
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            // inserting elements to array b
+            for (int i = 0; i < m; i++) {
+                b[i] = sc.nextInt();
+            }
+            Solution ob = new Solution();
+            // calling NumberofElementsInIntersection method
+            // and printing the result
+            System.out.println(ob.NumberofElementsInIntersection(a, b, n, m));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to return the count of the number of elements in
+    // the intersection of two arrays.
+    public static int NumberofElementsInIntersection(int a[], int b[], int n, int m) {
+        // Your code here
+        HashSet<Integer> st=new HashSet<>();
+       for(int i:a){
+           st.add(i);
+       }
+       ArrayList<Integer> l=new ArrayList<>();
+       for(int i:b){
+           if(st.contains(i)){
+               l.add(i);
+               st.remove(i);
+           }
+       }
+       int res=0;
+       for(int i=0;i<l.size();i++){
+           res++;
+           
+       }
+       return res;
+    }
+};
