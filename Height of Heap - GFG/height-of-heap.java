6035b80a082aba97a8383772779bf710603eada5
Abhinav Ashish <94304669+Abhinavashish441@users.noreply.github.com>
2022-06-14 09:36:01 +0530

    Added solution - LeetHub

---
+++ Height of Heap - GFG/height-of-heap.java
@@ -0,0 +1,33 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG{
+    public static void main(String args[])throws IOException{
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(read.readLine());
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                arr[i] = Integer.parseInt(input_line[i]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.heapHeight(N, arr));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int heapHeight(int N, int arr[]){
+        // code here
+        return (int)Math.ceil((Math.log(N+1)/Math.log(2)))-1;
+    }
+}
